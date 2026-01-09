e8b1a3c480aef671c2c79268216a125fe5f64acd
Linh Ho <45103089+LinhHo@users.noreply.github.com>
2026-01-09 15:48:16 +0100

    Rename coordinates atlas to match ERA5

--- src/abundance_atlas.py
+++ src/abundance_atlas.py
@@ -2,9 +2,9 @@
 Resample atlas data to reference grid
 """
 
-
 import xarray as xr
 import rioxarray as rxr
+import numpy as np
 from geo_processing import clip_and_resample, create_reference_raster
 
 
@@ -14,9 +14,21 @@ def resample_atlas(bounds, paths, resolution):
 
     solar = rxr.open_rasterio(paths["solar_atlas"]).squeeze()
     # Convert from Photovoltaic power potential [kWh/kWp] average daily totals to capacity factor
-    ds["solar_CF"] = clip_and_resample(solar, ref, bounds) / 24 
+    solar_resampled = clip_and_resample(solar, ref, bounds) / 24
+    # rename coordinates to ERA5 standard
+    ds["solar_CF"] = solar_resampled.rename({"x": "longitude", "y": "latitude"})
 
     wind = rxr.open_rasterio(paths["wind_atlas"]).squeeze()
-    ds["wind_CF"] = clip_and_resample(wind, ref, bounds)
+    wind_resampled = clip_and_resample(wind, ref, bounds)
+    # rename coordinates to ERA5 standard
+    ds["wind_CF"] = wind_resampled.rename({"x": "longitude", "y": "latitude"})
+
+    # Ensure the output matches the exact data types and names of the ERA5 tiles
+    ds = ds.assign_coords(
+        {
+            "longitude": ds.longitude.astype(np.float32),
+            "latitude": ds.latitude.astype(np.float32),
+        }
+    )
 
     return ds
