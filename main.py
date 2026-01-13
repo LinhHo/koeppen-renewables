48c261fc182a06b972686dfbaaab00c3d1b0cd58
Linh Ho <45103089+LinhHo@users.noreply.github.com>
2026-01-13 11:16:28 +0100

    Combine to one output file

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ def main():
         if not out_file.exists():
             print(f"\n--- Processing Tile: {tile_str} ---")
             try:
-                # 1. Atlas
+                # 1. Atlas (abundance)
                 print("  -> Resampling Atlas...")
                 ds_main = resample_atlas(tile, PATHS, resolution=REFERENCE_RESOLUTION)
 
@@ -90,6 +90,11 @@ def main():
                         ],
                         join="override",  # ignore slight coordinate mismatches
                     )
+                # 3. Demand Potential
+                # incl. demand_potential, demand_temperature_induced, demand_settlement_proximity
+                print("  -> Computing demand potential...")
+                ds_demand = run_demand_potential_for_tile(tile, PATHS)
+                ds_main = xr.merge([ds_main, ds_demand], join="override")
 
                 # Atomic Save (HPC Safe)
                 tmp_path = str(out_file) + ".tmp"
@@ -104,37 +109,35 @@ def main():
                 if "tmp_path" in locals() and os.path.exists(tmp_path):
                     os.remove(tmp_path)
         else:
-            print(
-                f"Processed atlas and variability for tile {tile_str} exists. Skipping."
-            )
+            print(f"Processed tile for {tile_str} exists. Skipping.")
 
-        # save demand separately for now
-        demand_outfile = (
-            output_dir / f"demand_potential_{tile_str}_{START_YEAR}_{END_YEAR}.nc"
-        )
+        # # save demand separately for now
+        # demand_outfile = (
+        #     output_dir / f"demand_potential_{tile_str}_{START_YEAR}_{END_YEAR}.nc"
+        # )
 
-        if not demand_outfile.exists():
+        # if not demand_outfile.exists():
 
-            print(f"\n--- Processing demand potential for Tile: {tile_str} ---")
-            try:
-                # 3. Demand Potential
-                print("  -> Computing demand potential...")
-                ds_demand = run_demand_potential_for_tile(tile, PATHS)
+        #     print(f"\n--- Processing demand potential for Tile: {tile_str} ---")
+        #     try:
+        #         # 3. Demand Potential
+        #         print("  -> Computing demand potential...")
+        #         ds_demand = run_demand_potential_for_tile(tile, PATHS)
 
-                # Atomic Save (HPC Safe)
-                tmp_path = str(demand_outfile) + ".tmp"
-                ds_demand.to_netcdf(tmp_path)
-                os.rename(tmp_path, demand_outfile)
-                print(f"  [SUCCESS] Saved to {demand_outfile.name}")
-                ds_demand.close()
-                del ds_demand
+        #         # Atomic Save (HPC Safe)
+        #         tmp_path = str(demand_outfile) + ".tmp"
+        #         ds_demand.to_netcdf(tmp_path)
+        #         os.rename(tmp_path, demand_outfile)
+        #         print(f"  [SUCCESS] Saved to {demand_outfile.name}")
+        #         ds_demand.close()
+        #         del ds_demand
 
-            except Exception as e:
-                print(f"  [ERROR] Compute demand for tile {tile_str} failed: {e}")
-                if "tmp_path" in locals() and os.path.exists(tmp_path):
-                    os.remove(tmp_path)
-        else:
-            print(f"Demand potential {tile_str} exists. Skipping.")
+        #     except Exception as e:
+        #         print(f"  [ERROR] Compute demand for tile {tile_str} failed: {e}")
+        #         if "tmp_path" in locals() and os.path.exists(tmp_path):
+        #             os.remove(tmp_path)
+        # else:
+        #     print(f"Demand potential {tile_str} exists. Skipping.")
         client.restart()
     client.close()
 
