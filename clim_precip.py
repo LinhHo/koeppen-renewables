2a7a85931ccf76042a4af12223bde7e1d68a49f6
Linh Ho <45103089+LinhHo@users.noreply.github.com>
2026-02-06 18:35:27 +0100

    Add skipping existing files

--- clim_precip.py
+++ clim_precip.py
@@ -26,27 +26,39 @@ tiles = generate_tiles(domain[0], domain[1], domain[2], domain[3], 20)
 for tile in tiles:
     # client.restart()
     tile_str = "_".join(map(str, tile))
-
-    # da = load_era5_variable("https://data.earthdatahub.destine.eu/era5/reanalysis-era5-single-levels-monthly-means-v0.zarr", variable, bounds, 1995, 2025)
-    # da = load_era5_variable(
-    #     ERA5_ZARR_URL_month, variable, tile, 1995, 2026, daily_sum=True
-    # )
-    da = load_era5_variable(ERA5_ZARR_URL, variable, tile, 1995, 2026, daily_sum=True)
-    print(da)
-    da = da.chunk(
-        {"valid_time": -1, "latitude": 10, "longitude": 10}
-    )  # Ensure time is in single chunk for deficit calculation
-
-    # clim = da.groupby("valid_time.dayofyear").mean("valid_time")
-    # yearmean = da.groupby("valid_time.year").sum("valid_time").mean("year")
-    yearmean = da.resample(valid_time="YE").sum().mean("valid_time")
-    yearmean.to_netcdf(
-        f"resources/automatic/tp/tmp_era5_precip_year_average_1995_2025_{tile_str}.nc",
-        mode="w",
+    out_path = (
+        f"resources/automatic/tp/tmp_era5_precip_year_average_1995_2025_{tile_str}.nc"
     )
+    if os.path.exists(out_path):
+        print(f"Precipitation tile for {tile_str} exists. Skipping.")
+    else:
+        print(f"Processing precipitation tile for {tile_str}...")
+
+        # da = load_era5_variable("https://data.earthdatahub.destine.eu/era5/reanalysis-era5-single-levels-monthly-means-v0.zarr", variable, bounds, 1995, 2025)
+        # da = load_era5_variable(
+        #     ERA5_ZARR_URL_month, variable, tile, 1995, 2026, daily_sum=True
+        # )
+        da = load_era5_variable(
+            ERA5_ZARR_URL, variable, tile, 1995, 2026, daily_sum=True
+        )
+        print(da)
+        da = da.chunk(
+            {"valid_time": -1, "latitude": 10, "longitude": 10}
+        )  # Ensure time is in single chunk for deficit calculation
+
+        # clim = da.groupby("valid_time.dayofyear").mean("valid_time")
+        # yearmean = da.groupby("valid_time.year").sum("valid_time").mean("year")
+        yearmean = da.resample(valid_time="YE").sum().mean("valid_time")
+        yearmean.to_netcdf(
+            out_path,
+            mode="w",
+        )
 
 ds = xr.open_mfdataset(
     "resources/automatic/tp/tmp_era5_precip_year_average_1995_2025_*.nc",
     combine="by_coords",
 )
-ds.to_netcdf("resources/automatic/tp/era5_precip_year_average_1995_2025.nc", mode="w")
+ds.to_netcdf(
+    "resources/automatic/tp/era5_total_precipitation_year_average_1995_2025.nc",
+    mode="w",
+)
